fbc7cffb85d8db93f6ac29e2306f10f0cbcc1785
ivorgilmartin <50912513+ivorgilmartin@users.noreply.github.com>
2021-04-05 08:08:27 +0100

    add index.js

--- instant-search/index.js
+++ instant-search/index.js
@@ -1,11 +1,11 @@
 import { data } from "./data.js";
 const list = document.querySelector("#list");
 const search = document.querySelector("#app-search");
-// this function is called when the user writes a character and its goal is to remove leading & trailing spaces as well as lower case the string so we can implement a case insensitive search
+
 const cleanupWord = (word) => {
   return word.toLowerCase().trim();
 };
-// this is the function that will perform the actual search
+
 const filterData = (word) => {
   return data.filter((item) => {
     return item.spacecraft.toLowerCase().includes(word);
@@ -22,9 +22,9 @@ const render = (word = "") => {
     list.insertAdjacentHTML("beforeend", `<li>${item.spacecraft}</li>`);
   });
 };
-// render on page load
+
 render();
-// render as the user types
+
 search.addEventListener("input", () => {
   render(search.value);
 });
